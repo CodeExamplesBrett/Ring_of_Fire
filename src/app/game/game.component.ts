e45bed9d878540076b37b9eae76002d86b3857b5
Brett Scott <brettsamscott@gmail.com>
2022-12-19 21:31:05 +0100

    refactoring, fix Angular material design button 2

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -105,7 +105,6 @@ export class GameComponent implements OnInit {
   }
 
 
-
   editPlayer(playerId){
     console.log('player', playerId)
     const dialogRef = this.dialog.open(EditPlayerComponent);
@@ -141,6 +140,7 @@ export class GameComponent implements OnInit {
     });
   }
 
+
   saveGame(){
     updateDoc(doc(this.firestore,'games', this.gameId), 
       this.game.toJson() 
@@ -149,47 +149,3 @@ export class GameComponent implements OnInit {
   };
 
 }
-
-
-
-
-
-
-
-
-
-/*export class PokedexFirestoreService {
-  private pokemonCollection: CollectionReference<DocumentData>;
-
-  constructor(private readonly firestore: Firestore) {
-    this.pokemonCollection = collection(this.firestore, 'pokemon');
-  }
-
-  getAll() {
-    return collectionData(this.pokemonCollection, {
-      idField: 'id',
-    }) as Observable<Pokemon[]>;
-  }
-
-  get(id: string) {
-    const pokemonDocumentReference = doc(this.firestore, `pokemon/${id}`);
-    return docData(pokemonDocumentReference, { idField: 'id' });
-  }
-
-  create(pokemon: Pokemon) {
-    return addDoc(this.pokemonCollection, pokemon);
-  }
-
-  update(pokemon: Pokemon) {
-    const pokemonDocumentReference = doc(
-      this.firestore,
-      `pokemon/${pokemon.id}`
-    );
-    return updateDoc(pokemonDocumentReference, { ...pokemon });
-  }
-
-  delete(id: string) {
-    const pokemonDocumentReference = doc(this.firestore, `pokemon/${id}`);
-    return deleteDoc(pokemonDocumentReference);
-  }
-}  */
